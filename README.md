6dfe76e9cc1cb2d68c597bac399bda3dd6bcaed9
ProcessVersion <51869374+ProcessVersion@users.noreply.github.com>
2021-02-24 21:38:12 -0500

    commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # local-api
+
+Using the help of [this](https://www.youtube.com/watch?v=25_RgVDGGuE) tutorial, I was able to start the "base" of an API
+
+## Endpoints
+
+Base URL: localhost:3000
+
+- [GET] `/fact` | Returns a fact 🧠
+
+Example response
+
+```json
+{
+	"fact": "Armadillo shells are bulletproof! So when the world goes to shit grab yourself some armadillo armour",
+	"source": "https://www.rd.com/list/interesting-facts/",
+	"url": "https://www.reddit.com/r/facts/comments/lqe1ed/armadillo_shells_are_bulletproof_so_when_the/?utm_source=share&utm_medium=web2x&context=3",
+	"success": true,
+	"error": false
+}
+```
+
+- [GET] `/roast` | Returns a nice roast 🔥
+- [GET] `/pickup` | A nice pickup line 😏
+- [GET] `/joke` | A joke 😂
--- index.js
+++ index.js
@@ -4,16 +4,31 @@ const app = express();
 const { facts, sources, url } = require("./facts.json");
 
 app.get("/fact/", (req, res) => {
-	const ranNum = Math.floor(Math.random() * facts.length);
-	const number = ranNum;
-	const obj = {
-		fact: facts[number],
-		source: sources[number],
-		url: url[number],
-		success: true,
-		error: false,
-	};
-	return res.status(200).send(obj);
+	try {
+		const ranNum = Math.floor(Math.random() * facts.length);
+		const number = ranNum;
+		const obj = {
+			fact: facts[number],
+			source: sources[number],
+			url: url[number],
+			success: true,
+			error: false,
+		};
+		return res.status(200).send(obj);
+	} catch (e) {
+		const obj = {
+			fact: null,
+			source: null,
+			url: null,
+			success: false,
+			error: true,
+		};
+		return res.status(500).send(obj);
+	}
+});
+
+app.use((req, res) => {
+	res.status(404).send("Oops, this route is invalid!");
 });
 
 app.listen(process.env.PORT, () =>
